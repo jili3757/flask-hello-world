bde6eb634e8ed21971f53182da77049f0a476fc5
jili3757 <156278040+jili3757@users.noreply.github.com>
2025-11-07 00:53:46 -0700

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,12 @@ def testing():
     conn = psycopg2.connect("postgresql://jinny_render_db_user:LWmku3hpecib7ktldE2aG4F7tF8SEUGa@dpg-d46pkh6r433s738c7bhg-a/jinny_render_db")
     conn.close()
     return "Database Connection Sucessful"
+
+@app.route('/db_create')
+def creating():
+    conn = psycopg2.connect("postgresql://jinny_render_db_user:LWmku3hpecib7ktldE2aG4F7tF8SEUGa@dpg-d46pkh6r433s738c7bhg-a/jinny_render_db")
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS Basketball( First varchar(255), Last varchar(255), City varchar(255), Name varchar(255), Number int);''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Sucessfully Created"
